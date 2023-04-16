82fa44e97441d3ec1768c7c0451a21330ecab587
InkyBox <85509162+kittstone@users.noreply.github.com>
2023-04-16 17:47:17 -0500

    MY OCK

---
+++ modswitcher/data/states/ModSwitcher.hx
@@ -0,0 +1,17 @@
+import haxe.io.Path;
+import funkin.assets.ModsFolder;
+import sys.FileSystem;
+
+var mods:Array<String> = [];
+function create() {
+	for(modFolder in FileSystem.readDirectory(ModsFolder.modsPath)) {
+		if (FileSystem.isDirectory(ModsFolder.modsPath+modFolder)) {
+			mods.push(modFolder);
+		}
+	}
+	trace(mods);
+}
+
+function update(elapsed:Float) {
+	if (FlxG.keys.justPressed.NUMPADMULTIPLY) FlxG.switchState(new ModState('ModSwitcher'));
+}
