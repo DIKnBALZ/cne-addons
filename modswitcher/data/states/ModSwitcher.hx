b1735dfc3d7b0820b513a6d7d499ad76df9d5f16
WizardMantis441 <77557487+WizardMantis441@users.noreply.github.com>
2023-04-16 18:55:50 -0500

    Co-authored-by: inkybox <kittstone@users.noreply.github.com>

--- modswitcher/data/states/ModSwitcher.hx
+++ modswitcher/data/states/ModSwitcher.hx
@@ -1,21 +1,38 @@
 import haxe.io.Path;
 import funkin.assets.ModsFolder;
 import sys.FileSystem;
+import flixel.FlxSprite;
+import funkin.assets.Paths;
+import funkin.menus.MainMenuState;
 
-var mods:Array<String> = [];
-function create() {
-	for(modFolder in FileSystem.readDirectory(ModsFolder.modsPath)) {
-		if (FileSystem.isDirectory(ModsFolder.modsPath+modFolder)) {
-			mods.push(modFolder);
+class ModSwitcher extends funkin.editors.ui.UIState {
+	public var mods:Array<String> = [];
+	public var sprites:Array<FlxSprite> = [];
+	public function new() {
+		super();
+
+		for (modFolder in FileSystem.readDirectory(ModsFolder.modsPath)) {
+			if (FileSystem.isDirectory(ModsFolder.modsPath+modFolder)) {
+				mods.push(modFolder);
+			}
+		}
+		trace(mods);
+
+		for (i in mods) {
+			var spr:FlxSprite = new FlxSprite(360*(mods.indexOf(i))+50, 25).loadGraphic(Paths.image('what'));
+			spr.scale.set(0.5, 0.5);
+			spr.updateHitbox();
+			sprites.push(spr);
+			add(spr);
 		}
 	}
-	trace(mods);
 
-	for (i in mods) {
-		
+	public function update(elapsed:Float) {
+		super.update(elapsed);
+
+		if (FlxG.keys.justPressed.EIGHT)
+			FlxG.switchState(new ModSwitcher());
+		if (FlxG.keys.justPressed.ESCAPE)
+			FlxG.switchState(new MainMenuState());
 	}
-}
-
-function update(elapsed:Float) {
-	if (FlxG.keys.justPressed.NUMPADMULTIPLY) FlxG.switchState(new ModState('ModSwitcher'));
 }
