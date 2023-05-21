594f9d9f23d00953e48660281bf3d59a0d212d73
WizardMantis441 <77557487+WizardMantis441@users.noreply.github.com>
2023-05-21 18:00:03 -0500

    im at my dads house

---
+++ auto-chart/data/charts/mhm.hx
@@ -0,0 +1,35 @@
+// this will def not work but fuck it we ball
+
+return;
+
+var isStreamsOnly:Bool = true;
+var noteCount:Int = 1000;
+var noteInterval:Int = 4;
+scrollSpeed = 3;
+
+var prevNote:Int = FlxG.random.int(0, 3);
+
+function create() {
+    var noteBG = new FlxSprite(FlxG.width / 2, 0).makeGraphic(1, 1, 0x8D000000);
+    noteBG.setGraphicSize(FlxG.width, FlxG.height);
+    noteBG.updateHitbox();
+    noteBG.cameras = [camHUD];
+    insert(members.indexOf(strumLines), noteBG);
+
+    PlayState.SONG.strumLines[1].notes = [];
+    for (i in 0...noteCount) {
+        if (i % noteInterval != 0) continue;
+        var newAutoNote = FlxG.random.int(0, 3, isStreamsOnly ? [prevNote] : []);
+        PlayState.SONG.strumLines[1].notes.push({
+            time: Conductor.stepCrochet * i,
+            id: newAutoNote,
+            type: 0,
+            sLen: 0
+        });
+        prevNote = newAutoNote;
+    }
+}
+
+function beatHit() {
+    FlxG.sound.play(Paths.sound("editors/charter/metronome"), 1);
+}
---
+++ wiz-swagoldicon/data/charts/yuppers.hx
@@ -0,0 +1,29 @@
+var altIcon:HealthIcon;
+
+function postCreate() {
+    altIcon = new HealthIcon('bf-thebetterversion', true);
+    altIcon.cameras = [camHUD];
+    add(altIcon);
+}
+
+function update(elapsed:Float) {
+    if (FlxG.keys.justPressed.NINE) {
+        FlxG.sound.play(Paths.sound('huzzah'), 1);
+        iconP1.visible = !iconP1.visible;
+        boyfriend.playAnim("hey", true);
+        FlxG.camera.zoom += 0.015;
+		camHUD.zoom += 0.03;
+    }
+
+    // icon recreation lolol
+    altIcon.visible = !iconP1.visible;
+    altIcon.setPosition(iconP1.x, iconP2.y);
+    altIcon.scale.set(lerp(iconP1.scale.x, 1, 0.33), lerp(altIcon.scale.y, 1, 0.33));
+	altIcon.updateHitbox();
+    altIcon.health = healthBar.percent / 100;
+}
+
+function beatHit(curBeat:Int) {
+    altIcon.scale.set(1.2, 1.2);
+    altIcon.updateHitbox();
+}
