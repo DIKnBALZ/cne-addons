669117f178ab0a56f55ec2f169fdfe8769583a8f
WizardMantis441 <77557487+WizardMantis441@users.noreply.github.com>
2023-06-18 21:13:29 -0500

    the webhook shit

--- webhook/data/charts/webhook.hx
+++ webhook/data/charts/webhook.hx
@@ -1,12 +1,58 @@
-// https://discord.com/api/webhooks/1116811674964467751/uGLPnYadfLC-r3sc7mV6cKMV57ra-rH_Xe_MmhnhAwf4Mv0hRpCycTEAOExFVHMScu_8
+// worlds longest lines :(
 
-import funkin.backend.system.Main;
 import sys.Http;
+import sys.io.File;
+import funkin.backend.system.Main;
+import haxe.Json;
+import Std;
+
+var oCheck = Json.parse(File.getContent("addons/options.json"));
+if (oCheck.webhook.toggle != true) return;
+
+var webhook = oCheck.webhook.url;
+var imageUrl = "iVBORw0KGgoAAAANSUhEUgAAAcIAAACWCAYAAABNcIgQAAAACXBIWXMAADXUAAA11AFeZeUIAAACa0lEQVR4nO3VMQ3CABQAUcAjChirh7EKcFIFHeukNUHyk957Cm6757Yf5wP+7Ld+pxO4ofdnmU7ghl7TAQAwyQgBSDNCANKMEIA0IwQgzQgBSDNCANKMEIA0IwQgzQgBSDNCANKMEIA0IwQgzQgBSDNCANKMEIA0IwQgzQgBSDNCANKMEIA0IwQgzQgBSDNCANKMEIA0IwQgzQgBSDNCANKMEIA0IwQgzQgBSDNCANKMEIA0IwQgzQgBSDNCANKMEIA0IwQgzQgBSDNCANKMEIA0IwQgzQgBSDNCANKMEIA0IwQgzQgBSDNCANKMEIA0IwQgzQgBSDNCANKMEIA0IwQgzQgBSDNCANKMEIA0IwQgzQgBSDNCANKMEIA0IwQgzQgBSDNCANKMEIA0IwQgzQgBSDNCANKMEIA0IwQgzQgBSDNCANKMEIA0IwQgzQgBSDNCANKMEIA0IwQgzQgBSDNCANKMEIA0IwQgzQgBSDNCANKMEIA0IwQgzQgBSDNCANKMEIA0IwQgzQgBSDNCANKMEIA0IwQgzQgBSDNCANKMEIA0IwQgzQgBSDNCANKMEIA0IwQgzQgBSDNCANKMEIA0IwQgzQgBSDNCANKMEIA0IwQgzQgBSDNCANKMEIA0IwQgzQgBSDNCANKMEIA0IwQgzQgBSDNCANKMEIA0IwQgzQgBSDNCANKMEIA0IwQgzQgBSDNCANKMEIA0IwQgzQgBSDNCANKMEIA0IwQgzQgBSDNCANKMEIA0IwQgzQgBSDNCANKMEIA0IwQgzQgBSDNCANKMEIA0IwQgzQgBSDNCANKMEIA0IwQg7QLNjQetpPY3DwAAAABJRU5ErkJggg==";
+
+var currentID = "";
+
+function postCreate() {
+	Main.execAsync(()->{
+		var http = new Http(webhook + "?wait=true");
+		http.setParameter('content', '# ' + oCheck.webhook.name + ' started **' + formatText(PlayState.SONG.meta.displayName) + '**');
+		http.setParameter('flags', 4096);
+		http.onData = function(data) {
+			var json = Json.parse(data);
+			currentID = json.id;
+		};
+		http.request(true);
+	});
+}
+
+function beatHit() {
+    Main.execAsync(()->{
+        var http = new Http(webhook + "/messages/" + currentID);
+        http.setParameter('content', '## [' + oCheck.webhook.name + '] ' + formatText(PlayState.SONG.meta.displayName) + ' - (' + CoolUtil.timeToStr(Conductor.songPosition)+ ' / ' + CoolUtil.timeToStr(FlxG.sound.music.length) + ')\nAccuracy: `' + ((FlxMath.roundDecimal(accuracy * 100, 2) == -100) ? "N/A" : FlxMath.roundDecimal(accuracy * 100, 2) + '% (' + curRating.rating + ')') + '` // Misses: `' + misses + '` // Score: `' + songScore + '`');
+        http.customRequest(true, null, null, "PATCH");
+    });
+}
 
 function onSongEnd() {
-    Main.execAsync(()->{
-        var http = new Http('https://discord.com/api/webhooks/1116811674964467751/uGLPnYadfLC-r3sc7mV6cKMV57ra-rH_Xe_MmhnhAwf4Mv0hRpCycTEAOExFVHMScu_8');
-        http.setParameter('content', '**' + PlayState.SONG.meta.displayName + '** results:\nAccuracy: `' + FlxMath.roundDecimal(accuracy * 100, 2) + '% ('+ curRating.rating +')` - Misses: `' + misses + '` - Score: `' + songScore + '`');
-        http.request(true);
-    });
+	Main.execAsync(()->{
+		var http = new Http(webhook + "/messages/" + currentID);
+		http.setParameter('content', '# ' + formatText(PlayState.SONG.meta.displayName) + '** - Final Results\nAccuracy: `' + FlxMath.roundDecimal(accuracy * 100, 2) + '% ('+ curRating.rating +')` // Misses: `' + misses + '` // Score: `' + songScore + '`');
+		http.customRequest(true, null, null, "PATCH");
+	});
+}
+
+function formatText(str:String) {
+    var string = str;
+    string = StringTools.replace(string, "-", " ");
+    
+    var newString = "";
+
+    for (charI in 0...string.length) {
+        var char = string.charAt(charI);
+        newString += (charI == 0 || string.charAt(charI - 1) == " ") ? char.toUpperCase() : char;
+    }
+
+    return newString;
 }
